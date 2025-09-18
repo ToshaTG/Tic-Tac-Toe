576f5f2725ef05015ba0f95a349291da43e677ed
ToshaTG <antoanela2005@gmail.com>
2025-09-18 19:46:47 +0300

    Player.hpp

--- Include/Player.hpp
+++ Include/Player.hpp
@@ -1,15 +1,3 @@
 #pragma once
-
-namespace ttt {
-
-/// Reprezintă starea unei celule / jucătorul curent.
-enum class Player {
-    None = 0,
-    X,
-    O
-};
-
-/// Comută jucătorul: X -> O, O -> X, None rămâne None.
-Player Next(Player p) noexcept;
-
-} // namespace ttt
+enum class Player
+ { None = 0, X, O};
